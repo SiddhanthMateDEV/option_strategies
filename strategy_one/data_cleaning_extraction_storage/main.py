7efa953d6fd201a7a95f728e1dcc7f82340b56a6
Siddhanth Mate <siddhanthmate@Siddhanths-MacBook-Pro.local>
2024-06-06 18:10:58 +0530

    fixed mongodb

--- strategy_one/data_cleaning_extraction_storage/main.py
+++ strategy_one/data_cleaning_extraction_storage/main.py
@@ -6,7 +6,7 @@ import subprocess
 from pymongo import MongoClient
 from datetime import datetime
 import time
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 import logging
 
 logging.basicConfig(filename='database_creation.log', 
@@ -14,11 +14,17 @@ logging.basicConfig(filename='database_creation.log',
                     format='%(asctime)s | %(levelname)s | %(message)s')
 
 def start_mongodb(db_path):
-    subprocess.Popen(['mongod','--dbpath',db_path])
-    time.sleep(5)
+    try:
+        subprocess.Popen(['mongod','--dbpath',db_path])
+        time.sleep(5)
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error starting MongoDB: {e}")
 
 def stop_mongodb():
-    subprocess.Popen(['mongo','--eval','db.adminCommand({shutdown: 1})'])
+    try:
+        subprocess.Popen(['mongosh','--eval','db.getSiblingDB("admin").shutdownServer()'])
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error stopping MongoDB: {e}")
 
 def get_files(root_path):
     # Get a list of the file names in the root directory
@@ -27,31 +33,6 @@ def get_files(root_path):
     return [file_[:-4] for file_ in os.listdir(root_path) if file_.endswith(".csv")]
 
 
-def extract_attributes_of_file(file):
-    # for bookeeping purposes lets do some extra steps
-    '''
-    for example what were doing here is that i have files 
-    in the form of this string path: "/Users/siddhanthmate/Desktop/AllFiles/CODE/WORK_CODE/fintech/DATA/options_data/2022/14112022.csv"
-    i want the variables of day month and year as a string
-    '''
-    if file is None:
-        raise ValueError("File and collection_name must be specified")
-    
-
-    day = file[:2]
-    month = file[2:4]
-    year = file[4:]
-
-    '''
-    returning a dictionary of this will be more effecient i think if i am going to run this 
-    in a multiprocess fashion 
-    '''
-    return {
-        "day": day,
-        "month": month,
-        "year": year,
-        "file": file
-    }
 class database_creation:
     def __init__(self, 
                  db_path = None,
@@ -65,64 +46,83 @@ class database_creation:
         self.root_path = root_path
         self.db_path = db_path
         self.collection_name = collection_name
+        self.file = None
+        self.day = None
+        self.month = None
+        self.year = None
         self.file_attr = {}
         self.client = MongoClient("mongodb://localhost:27017/")
         logging.info(f"Initialised database_creation with db_path")
-    
-    def insert_database(self, 
-                        day = None, 
-                        month = None,
-                        year = None,
-                        file = None
-                        ):
+
+    def extract_attributes_of_file(self,file):
+        # for bookeeping purposes lets do some extra steps
+        '''
+        for example what were doing here is that i have files 
+        in the form of this string path: "/Users/siddhanthmate/Desktop/AllFiles/CODE/WORK_CODE/fintech/DATA/options_data/2022/14112022.csv"
+        i want the variables of day month and year as a string
+        '''
+        if file is None:
+            raise ValueError("File and collection_name must be specified")
         
-        data_obj = datetime.strptime(fr"{day}{month}{year}","%d%m%y")
-        
-        data_str = fr"{day}-{month}-{year}"
+        self.file = file
+
+        self.day = self.file[:2]
+        self.month = self.file[2:4]
+        self.year = self.file[4:]
+
+
+    def insert_database(self):
+                
+        data_str = fr"{self.day}_{self.month}_{self.year}"
         db_name = fr"db_{data_str}"
 
-        # read the csv data file
-        df = pd.read_csv(fr"{self.root_path}/{file}.csv")
-        mongo_uri = "mongodb://localhost:5000/"
-        client = MongoClient(mongo_uri)
-        db = client[str(db_name)]
-        collection = db[str(self.collection_name)]
-        data_dict = df.to_dict("records")
-        collection.insert_many(data_dict)
+        df = pd.read_csv(fr"{self.root_path}/{self.file}.csv")
+        
+        with self.client:
+            db = self.client[str(db_name)]
+            collection = db[str(self.collection_name)]
+            data_dict = df.to_dict("records")
+            result = collection.insert_many(data_dict)
+
         logging.info(f"Data entered: {db_name}")
         print(fr"Data for the {db_name} Entered Succesfully")
 
 
-    def create_database(self, 
-                        file):
+    def create_database(self,file = None):
         
         try:
-            file_attr_dictionary = extract_attributes_of_file(file)
-            self.insert_database(**file_attr_dictionary)
+            self.extract_attributes_of_file(file = file)
+            logging.debug("Attributes of the file are extracted")
+            self.insert_database()
+            logging.info("Database inserted into the collection")
         except Exception as e:
-            logging.info(f"Error Creating database for file {file}: {e}")
+            logging.error(f"Error Creating database for file {self.file}: {e}")
 
 def main():
     root_dir = fr"/Users/siddhanthmate/Desktop/AllFiles/CODE/WORK_CODE/fintech/DATA/options_data/2022"
-    file = get_files(root_path = root_dir)
+    files_ = get_files(root_path = root_dir)
 
     # here im putting the upper limit of cpu usage adjust this accordingly
     cpu_no_ = os.cpu_count()
 
     class_attr = {
         "collection_name": "options_data",
-        "db_path": fr"/Users/siddhanthmate/Desktop/AllFiles/CODE/WORK_CODE/fintech/options_strategies/database",
+        "db_path": fr"/usr/local/var/mongodb",
         "root_path": root_dir
     }
 
-    # pre initialise the class 
-    db_creator = database_creation(**class_attr)
+    # start_mongodb(class_attr["db_path"])
+
 
     try:
-        with ProcessPoolExecutor(max_workers = cpu_no_) as executor:
-            executor.map(db_creator.create_database,file)
+        with ThreadPoolExecutor() as executor:
+            db_creator = database_creation(**class_attr)
+            executor.map(db_creator.create_database,files_)
     except Exception as e:
-        logging.error(f"Error in ProcessPoolExecutor: {e}")
+        logging.error(f"Error in ThreadPoolExecutor: {e}")
+    # finally:
+    #     stop_mongodb()
+
 
 if __name__=="__main__":
     main()
